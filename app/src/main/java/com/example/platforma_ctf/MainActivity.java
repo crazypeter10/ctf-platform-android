980073b501cb78ac5ba1ff8d899fd3029dc11e76
vasian petru <vasian.petru.150@gmail.com>
2023-04-27 13:49:53 +0300

    google map api

--- app/src/main/java/com/example/platforma_ctf/MainActivity.java
+++ app/src/main/java/com/example/platforma_ctf/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.platforma_ctf;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +25,29 @@ public class MainActivity extends AppCompatActivity {
         DatabaseHelper dbHelper = new DatabaseHelper(this);
 
         Button loginButton = findViewById(R.id.login_button);
-      //  Button registerButton = findViewById(R.id.register_button);
 
         loginButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
         });
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        // Add a marker to the academy location
+        LatLng academyLocation = new LatLng( 44.4180219, 26.0859072);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(academyLocation);
+        markerOptions.title("Military Technical Academy Ferdinand I");
+        map.addMarker(markerOptions);
+
+        // Move the camera to the academy location
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(academyLocation, 17));
     }
 }
